b4aa4eb35ee3ea9c9c83e4e2e4e78c7f318634b3
Nestor Tamares <jrtamares@gmail.com>
2018-11-12 22:20:31 -0600

    Cleaned up styling

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express           = require("express"),
     app               = express(),
     bodyParser        = require ("body-parser"),
     mongoose          = require("mongoose"),
-    methodOverride    = require("method-override");
+    methodOverride    = require("method-override"),
+    expressSanitizer  = require("express-sanitizer");
 
 mongoose.connect("mongodb://localhost/restful_blog_app", {useNewUrlParser: true});
 mongoose.set("useFindAndModify", false);
@@ -11,6 +12,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 
 var blogSchema = new mongoose.Schema({
@@ -50,6 +52,7 @@ app.get("/blogs/new", function(req,res){
 
 // CREATE ROUTE
 app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -86,6 +89,7 @@ app.get("/blogs/:id/edit", function(req,res){
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if(err){
             res.redirect("/blogs");
